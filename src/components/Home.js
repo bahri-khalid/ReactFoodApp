3553b7c666444cef049c3e0e2bdc331410ba6502
khalid <khalidensamc@gmail.com>
2021-12-31 23:39:21 +0100

    mlkjdqf

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import { FaSearch } from "react-icons/fa";
-import SearchBar from "./SearchBar";
+import { useState } from "react";
 import {
     BrowserRouter as Router,
     Switch,
@@ -7,39 +7,48 @@ import {
     Link
   } from "react-router-dom";
 import Meal from "./Meal";
+import Menu from "./Menu";
 const Home = ({menu}) => {
-    const { search } = window.location;
-    const query = new URLSearchParams(search).get('s');
-  const filterMeals = (meals, query) => {
-    if (!query) {
-        return meals;
-    }
+  const [input,setInput] = useState("")
+  const [toDisplay,setToDisplay] = useState([])
 
-    return meals.meals.filter((meal) => {
-        const mealName = meal.strMeal.toLowerCase();
-        return mealName.includes(query.toLowerCase());
-    });
-  };
-
-    let test = filterMeals(menu,query)
-    console.log(test.meals)
+  const handleChange =(typed)=>{
+      let matched =[];
+      if(typed){
+          matched = menu.meals.filter((dt)=>{
+              return dt.strMeal.match(new RegExp(`${typed}`,'gi')) || dt.strCategory.match(new RegExp(`${typed}`,'gi')) ||dt.strArea.match(new RegExp(`${typed}`,'gi'))
+          })
+      }
+      setInput(typed);
+      setToDisplay(matched)
+  }
     return (
         <div className="home">
             <div className="main">
                 <h1>Good food choices are <br/>good investments.</h1>
                 <p>There is a powerful need for symbolism, and that means the architecture must have something that appeals to the human heart.  </p>
                 <div className="Search">
-                <SearchBar data={test.meals}/>
-               
+        <label htmlFor="header-search">
+            <span className="visually-hidden">Search blog posts</span>
+        </label>
+        <input
+            type="text"
+            id="header-search"
+            placeholder="Search blog posts"
+            name="s" 
+            value={input}
+            onChange={(e)=>{handleChange(e.target.value)}}
+        />
+        <button  type="submit"><FaSearch/></button>
+        </div>
                 </div>
                 <div className="btn">
                     <button className="btnMenu">Menu</button>
                     <span>Watch our menu</span>
                 </div>
+                <Menu meals={toDisplay}/>
             </div>
-            
-        </div>
       );
 }
- 
+
 export default Home;
